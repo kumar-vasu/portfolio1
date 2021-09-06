b0198933eb80deccf0a0a781c27c2ae595f75f51
kumar-vasu <75114390+kumar-vasu@users.noreply.github.com>
2021-09-06 23:13:46 +0530

    compressed js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+const navToggle=document.querySelector(".nav-toggle"),navLinks=document.querySelectorAll(".nav__link");navToggle.addEventListener("click",()=>{document.body.classList.toggle("nav-open")}),navLinks.forEach(e=>{e.addEventListener("click",()=>{document.body.classList.remove("nav-open")})});
